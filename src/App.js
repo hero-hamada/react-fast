720bc41e8522a0c55bdb2d448eed8b7536b400dd
hero-hamada <merey_zhan99@mail.ru>
2021-10-17 01:22:07 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import TodoList from './Todo/TodoList';
+import AddTodo from './Todo/AddTodo';
+import Context from './context';
+import Modal from './Modal/Modal';
+
 
 function App() {
+  const [todos, setTodos] = React.useState([
+    {id: 1, completed: false, title: 'Buy bread'},
+    {id: 2, completed: false, title: 'Buy milk'},
+    {id: 3, completed: false, title: 'Buy apple'}
+  ])
+  // let todos = 
+
+  useEffect(() => {
+    
+  }, [])
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      }) 
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([
+      {
+        title,
+        id: Date.now(),
+        completed: false
+      }
+    ]))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{removeTodo}}>
+      <div className="wrapper">
+        <h1>Todo App</h1>
+
+        <Modal/>
+
+        <AddTodo onCreate={addTodo}/>
+        {todos.length ? (<TodoList todos={todos} onToggle={toggleTodo}/>) : (<p>No todos!</p>)}
+
+      </div>
+    </Context.Provider>
   );
 }
 
